84ae4e42c9c5cef0305092409939268ff148c2a7
David Bitterlich <david-bitterlich@t-online.de>
2026-05-11 22:12:59 +0200

    Remove lsign-key

--- build_files/surface.sh
+++ build_files/surface.sh
@@ -4,8 +4,6 @@ set -euxo pipefail
 curl -s https://raw.githubusercontent.com/linux-surface/linux-surface/master/pkg/keys/surface.asc \
     | pacman-key --add -
 
-pacman-key --lsign-key 56C464BAAC421453
-
 cat >> /etc/pacman.conf <<EOF
 
 [linux-surface]
